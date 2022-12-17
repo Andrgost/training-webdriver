54693f6739f94124ef37b95b9b14ac1cd0546e37
Aleksandr Gostev <aleksandr_gostev@epam.com>
2022-12-17 15:21:10 +0500

    Parallel tests via testng

--- task_three_and_four/src/test/java/com/epam/webdriver/aleksandrgostev/test/CloudGoogleHomePageTest.java
+++ task_three_and_four/src/test/java/com/epam/webdriver/aleksandrgostev/test/CloudGoogleHomePageTest.java
@@ -4,9 +4,9 @@ import com.epam.webdriver.aleksandrgostev.driver.DriverManager;
 import com.epam.webdriver.aleksandrgostev.pages.googlecloud.CloudGoogleHomePage;
 import com.epam.webdriver.aleksandrgostev.pages.googlecloud.EstimatePage;
 import com.epam.webdriver.aleksandrgostev.pages.googlecloud.GoogleCloudPricingCalculatorPage;
-import com.epam.webdriver.aleksandrgostev.testdata.ParametersByDataProvider;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -15,16 +15,17 @@ import static java.lang.String.format;
 public class CloudGoogleHomePageTest {
 
     private static final String ERROR_MESSAGE = "%s is not as expected";
-    private final DriverManager driverManager = new DriverManager();
 
     @BeforeMethod(alwaysRun = true)
     public void browserSetup() {
-        driverManager.createWebDriver();
+        DriverManager.getDriver();
     }
 
-    @Test(description = "Verify Datacenter Location field",
-            dataProvider = "fieldValuesDataProvider",
-            dataProviderClass = ParametersByDataProvider.class)
+    @Test(description = "Verify Datacenter Location field")
+    @Parameters({"searchTerm", "numberOfInstances", "operatingSystemSoftware",
+            "provisioningModel", "machineType", "gpuType",
+            "numberOfGPU", "localSSD", "datacenterLocation",
+            "datacenterLocationExpected", "machineTypeExpected", "totalPriceExpected"})
     public void checkDatacenterLocation(String searchTerm,
                                         int numberOfInstances,
                                         String operatingSystemSoftware,
@@ -37,7 +38,7 @@ public class CloudGoogleHomePageTest {
                                         String datacenterLocationExpected,
                                         String machineTypeExpected,
                                         String totalPriceExpected) {
-        GoogleCloudPricingCalculatorPage calculatorPage = new CloudGoogleHomePage(driverManager.getDriver())
+        GoogleCloudPricingCalculatorPage calculatorPage = new CloudGoogleHomePage(DriverManager.getDriver())
                 .openPage()
                 .searchForTerm(searchTerm)
                 .clickSearchResult();
@@ -64,6 +65,6 @@ public class CloudGoogleHomePageTest {
 
     @AfterMethod(alwaysRun = true)
     public void browserTearDown() {
-        driverManager.quitWebDriver();
+        DriverManager.quitWebDriver();
     }
 }
