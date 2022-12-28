21e6bf08c50111a30f1b730676c2d58f35c29b47
Aleksandr Gostev <aleksandr_gostev@epam.com>
2022-12-28 15:29:49 +0500

    Run xml with params - upd DriverManager

--- task_three_and_four/src/test/java/com/epam/webdriver/aleksandrgostev/driver/DriverManager.java
+++ task_three_and_four/src/test/java/com/epam/webdriver/aleksandrgostev/driver/DriverManager.java
@@ -3,6 +3,7 @@ package com.epam.webdriver.aleksandrgostev.driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
@@ -16,17 +17,31 @@ public class DriverManager {
     }
 
     public static WebDriver getDriver() {
+        createDriver();
+        return driver.get();
+    }
+
+    public static void quitWebDriver() {
+        Optional.ofNullable(driver.get()).ifPresent(d -> {
+            driver.get().quit();
+            driver.set(null);
+        });
+    }
+
+    private static void createDriver() {
         if (null == driver.get()) {
-//            //How to run test with Param=kkk via Intellij IDEA?
-//            System.out.println(System.getProperty("kkk"));
-            switch (System.getProperty("browser")) {
+            switch (System.getProperty("browser", "chrome")) {
                 case "firefox": {
                     WebDriverManager.firefoxdriver().setup();
                     driver.set(new FirefoxDriver());
                 }
                 case "edge": {
                     WebDriverManager.edgedriver().setup();
-                    driver.set(new FirefoxDriver());
+                    driver.set(new EdgeDriver());
+                }
+                case "chrome": {
+                    WebDriverManager.chromedriver().setup();
+                    driver.set(new ChromeDriver());
                 }
                 default: {
                     WebDriverManager.chromedriver().setup();
@@ -36,13 +51,5 @@ public class DriverManager {
             driver.get().manage().window().maximize();
             driver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
         }
-        return driver.get();
-    }
-
-    public static void quitWebDriver() {
-        Optional.ofNullable(driver.get()).ifPresent(d -> {
-            driver.get().quit();
-            driver.set(null);
-        });
     }
 }
